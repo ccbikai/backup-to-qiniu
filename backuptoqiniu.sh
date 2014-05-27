e769743ab3940804ca60bf8e8e6f3480844eadd1
Kslr <kslrwang@gmail.com>
2014-05-27 16:14:49 +0800

    增加压缩文件加密
    增加压缩文件加密功能，默认密码为hello
    注：压缩方式由tar修改为zip（因为tar解压需要安装程序,zip原生支持）

--- backuptoqiniu.sh
+++ backuptoqiniu.sh
@@ -18,6 +18,8 @@ MYSQL_PASS="mysqlpassword"
 MYSQL_DBS="dbname"
 # 备份文件临时存放目录，一般不需要更改
 BACKUP_DIR="/tmp/backuptoqiniu/"
+# 备份文件压缩密码
+BACKUP_FILE_PASSWD="hello"
 
 ## 备份配置信息 End ##
 
@@ -49,8 +51,8 @@ echo "dump ok"
 
 # 打包
 echo "start tar"
-BACKUP_FILENAME="$BACKUP_NAME-backup-$NOW.tar.gz"
-tar -cPzf $BACKUP_DIR/$BACKUP_FILENAME $BACKUP_DIR/*.sql $BACKUP_SRC
+BACKUP_FILENAME="$BACKUP_NAME-backup-$NOW.zip"
+zip -rqp $BACKUP_FILE_PASSWD $BACKUP_DIR/$BACKUP_FILENAME $BACKUP_DIR/*.sql $BACKUP_SRC
 echo "tar ok"
 
 # 上传
